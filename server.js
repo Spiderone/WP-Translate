9a395f952959e19c0243b5da8fd2a45f9b91ce4d
Spiderone <iamspiderone@gmail.com>
2024-04-30 06:25:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,23 +8,26 @@ const DEEPL_API_KEY = '0f1efd94-f201-45eb-937f-062d1cf120a7:fx';
 const DEEPL_BASE_URL = 'https://api-free.deepl.com/v2/translate';
 
 // Middleware to enable CORS (Cross-Origin Resource Sharing)
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from all origins
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specified HTTP methods
-    res.setHeader('Access-Control-Allow-Headers', '*'); // Allow specified headers
-    res.setHeader('Access-Control-Max-Age', '3600'); // Cache preflight requests for 1 hour
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
     if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    } else {
-        next();
+        res.status(200).end();
+        return;
     }
-});
+    return await fn(req, res);
+};
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Endpoint to handle translation requests
-app.post('/translate', async (req, res) => {
+app.post('/translate', allowCors(async (req, res) => {
     // Check if request body exists and contains required properties
     if (!req.body || !req.body.text || !req.body.target_lang) {
         console.error('Invalid request body:', req.body);
@@ -49,7 +51,7 @@ app.post('/translate', async (req, res) => {
         console.error('Error translating text:', error);
         res.status(500).json({ error: 'Error translating text' });
     }
-});
+}));
 
 // Start the server
 app.listen(PORT, () => {
